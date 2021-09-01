922b20d804a7168466a393155cb6d6675d3113e2
ThirSight Zhang <thirsight@gmail.com>
2021-09-01 21:07:51 +0800

    update AirportTaipei Patterns Part

--- src/components/AirportTaipei.php
+++ src/components/AirportTaipei.php
@@ -12,6 +12,91 @@ use Exception;
 class AirportTaipei extends FlightCrawler
 {
     /**
+     * @return array
+     */
+    protected function getPatterns()
+    {
+        return [
+            // Status
+            'flight-status__title">(?<status>.*?)</div>[\s\S]*?',
+            // Air Company
+            'flight-airline__text">(?<company>.*?)</div>[\s\S]*?',
+            // Departure City
+            'Departure</div>[\s\S]*?flight-info__city">(?<dept>[\s\S]*?)</div>[\s\S]*?',
+            // Departure Scheduled Date
+            'flight-info__date">(?<deptSchedDate>[\s\S]*?)</div>[\s\S]*?',
+            // Departure Actual Time
+            'flight-info__infobox-text--\w+">(?<deptActualTime>[\s\S]*?)</div>[\s\S]*?',
+            // Departure Scheduled Time
+            'Scheduled Departure Time:(?<deptSchedTime>[\s\S]*?)</div>[\s\S]*?',
+            // Arrival City
+            'Arrival</div>[\s\S]*?flight-info__city">(?<dest>[\s\S]*?)</div>[\s\S]*?',
+            // Arrival Scheduled Date
+            'flight-info__date">(?<arriSchedDate>[\s\S]*?)</div>[\s\S]*?',
+            // Arrival Actual Time
+            'flight-info__infobox-text--\w+">(?<arriActualTime>[\s\S]*?)</div>[\s\S]*?',
+            // Arrival Scheduled Time
+            'Scheduled Arrival Time:(?<arriSchedTime>[\s\S]*?)</div>',
+        ];
+    }
+
+    /**
+     * @param string $html
+     * @return array
+     */
+    protected function parseHtmlPatterns(string &$html)
+    {
+        if (!preg_match('#' . implode('', $this->getPatterns()) . '#im', $html, $raw)) {
+            return [];
+        }
+        array_walk($raw, function (&$val) {
+            $val = trim($val);
+        });
+        // 實際出發時間 07:55 (2021-08-28)
+        if (preg_match('#(\d\d:\d\d)(?:\s+\((\d{4}-\d\d-\d\d)\))?#', $raw['deptActualTime'], $m)) {
+            $raw['deptActualTime'] = $m[1];
+            $raw['deptActualDate'] = $m[2] ?? $raw['deptSchedDate'];
+        }
+        // 實際抵達時間 07:55 (2021-08-28)
+        if (preg_match('#(\d\d:\d\d)(?:\s+\((\d{4}-\d\d-\d\d)\))?#', $raw['arriActualTime'], $m)) {
+            $raw['arriActualTime'] = $m[1];
+            $raw['arriActualDate'] = $m[2] ?? $raw['arriSchedDate'];
+        }
+        return $raw;
+    }
+
+    /**
+     * @param string $html
+     * @return array
+     */
+    protected function parseHtmlPatternsPart(string &$html)
+    {
+        $keepPattern = sprintf('#(?:%s)#', implode('|', ['<company>', '<dept>', '<deptSchedDate>', '<deptSchedTime>', '<dest>']));
+        $patterns = array_filter($this->getPatterns(), function ($p) use (&$keepPattern) {
+            return preg_match($keepPattern, $p);
+        });
+        if (!preg_match('#' . implode('', $patterns) . '#im', $html, $raw)) {
+            return [];
+        }
+        array_walk($raw, function (&$val) {
+            $val = trim($val);
+        });
+        // 表定出發時間 10:37 (2021-09-01)
+        if (preg_match('#(\d\d:\d\d)(?:\s+\((\d{4}-\d\d-\d\d)\))?#', $raw['deptSchedTime'], $m)) {
+            $raw['deptSchedTime'] = $m[1];
+            $raw['deptSchedDate'] = $m[2] ?? $raw['deptSchedDate'];
+        }
+        $raw['deptActualDate'] = '';
+        $raw['deptActualTime'] = '';
+        $raw['arriSchedDate']  = '';
+        $raw['arriSchedTime']  = '';
+        $raw['arriActualTime'] = '';
+        $raw['arriActualDate'] = '';
+        $raw['status'] = '';
+        return $raw;
+    }
+
+    /**
      * @param string $flightNo
      * @return array
      */
@@ -19,46 +104,14 @@ class AirportTaipei extends FlightCrawler
     {
         try {
             $resp = $this->client->get("https://www.taipei-airport.com/taoyuan-flight-arrival/{$flightNo}");
-            $patterns = [
-                // Air Company
-                'flight-status__title">(?<status>.*?)</div>[\s\S]*?',
-                // Air Company
-                'flight-airline__text">(?<company>.*?)</div>[\s\S]*?',
-                // Departure City
-                'Departure</div>[\s\S]*?flight-info__city">(?<dept>[\s\S]*?)</div>[\s\S]*?',
-                // Departure Scheduled Date
-                'flight-info__date">(?<deptSchedDate>[\s\S]*?)</div>[\s\S]*?',
-                // Departure Actual Time
-                'flight-info__infobox-text--\w+">(?<deptActualTime>[\s\S]*?)</div>[\s\S]*?',
-                // Departure Scheduled Time
-                'Scheduled Departure Time:(?<deptSchedTime>[\s\S]*?)</div>[\s\S]*?',
-                // Arrival City
-                'Arrival</div>[\s\S]*?flight-info__city">(?<dest>[\s\S]*?)</div>[\s\S]*?',
-                // Arrival Scheduled Date
-                'flight-info__date">(?<arriSchedDate>[\s\S]*?)</div>[\s\S]*?',
-                // Arrival Actual Time
-                'flight-info__infobox-text--\w+">(?<arriActualTime>[\s\S]*?)</div>[\s\S]*?',
-                // Arrival Scheduled Time
-                'Scheduled Arrival Time:(?<arriSchedTime>[\s\S]*?)</div>',
-            ];
-            if (!preg_match('#' . implode('', $patterns) . '#im', $resp->getBody()->getContents(), $raw)) {
+            $html = $resp->getBody()->getContents();
+            $raw = $this->parseHtmlPatterns($html) ?: $this->parseHtmlPatternsPart($html);
+            if (empty($raw)) {
                 return [];
             }
-            array_walk($raw, function (&$val) {
-                $val = trim($val);
-            });
-            // 實際出發時間 07:55 (2021-08-28)
-            if (preg_match('#(\d\d:\d\d)(?:\s+\((\d{4}-\d\d-\d\d)\))?#', $raw['deptActualTime'], $m)) {
-                $raw['deptActualTime'] = $m[1];
-                $raw['deptActualDate'] = $m[2] ?? $raw['deptSchedDate'];
-            }
-            // 實際抵達時間 07:55 (2021-08-28)
-            if (preg_match('#(\d\d:\d\d)(?:\s+\((\d{4}-\d\d-\d\d)\))?#', $raw['arriActualTime'], $m)) {
-                $raw['arriActualTime'] = $m[1];
-                $raw['arriActualDate'] = $m[2] ?? $raw['arriSchedDate'];
-            }
             // 日期是否與傳遞的日期一致
-            if ($raw['arriSchedDate'] != $date && $raw['arriActualDate'] != $date) {
+            if ($raw['deptSchedDate'] != $date && $raw['deptActualDate'] != $date
+                && $raw['arriSchedDate'] != $date && $raw['arriActualDate'] != $date) {
                 return [];
             }
             $flight = [
@@ -68,9 +121,9 @@ class AirportTaipei extends FlightCrawler
                 'dept'        => preg_match('#.*?\((\w+)\)#', $raw['dept'], $m) ? $m[1] : '',
                 'dest'        => preg_match('#.*?\((\w+)\)#', $raw['dest'], $m) ? $m[1] : '',
                 'etd'         => sprintf('%s %s:00', $raw['deptSchedDate'], $raw['deptSchedTime']),
-                'eta'         => sprintf('%s %s:00', $raw['arriSchedDate'], $raw['arriSchedTime']),
-                'atd'         => sprintf('%s %s:00', $raw['deptActualDate'], $raw['deptActualTime']),
-                'ata'         => sprintf('%s %s:00', $raw['arriActualDate'], $raw['arriActualTime']),
+                'eta'         => $raw['arriSchedDate'] ? sprintf('%s %s:00', $raw['arriSchedDate'], $raw['arriSchedTime']) : null,
+                'atd'         => $raw['deptActualDate'] ? sprintf('%s %s:00', $raw['deptActualDate'], $raw['deptActualTime']) : null,
+                'ata'         => $raw['arriActualDate'] ? sprintf('%s %s:00', $raw['arriActualDate'], $raw['arriActualTime']) : null,
                 'type'        => '',
                 'status'      => strtolower(trim(preg_replace('# - .*$#', '', $raw['status']))),
                 'statusBrief' => '',
